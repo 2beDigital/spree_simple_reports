a5b6f45b12019283611ae2ab08bb3894c332ec6b
Noel <noel@2bedigital.com>
2017-11-24 12:28:51 +0000

    added new reports,

--- app/controllers/spree/admin/reports_controller_decorator.rb
+++ app/controllers/spree/admin/reports_controller_decorator.rb
@@ -4,11 +4,14 @@ module Spree
       respond_to :html, :csv
       module SimpleReport
         def initialize
-          ReportsController.add_available_report!(:total_sales_of_each_product)
+          ReportsController.add_available_report!(:total_sales_of_each_variant)
+          ReportsController.add_available_report!(:total_sales_by_product)
           ReportsController.add_available_report!(:ten_days_order_count)
           ReportsController.add_available_report!(:thirty_days_order_count)
           ReportsController.add_available_report!(:stock_report)
           ReportsController.add_available_report!(:stockout_report)
+          ReportsController.add_available_report!(:reimbursement_total)
+          ReportsController.add_available_report!(:sales_total_net)         
           super
         end
       end
@@ -18,13 +21,24 @@ module Spree
       helper "spree/admin/simple_reports"
       include SimpleReportsHelper
 
-      def total_sales_of_each_product
+      def total_sales_of_each_variant
         @variants = Variant.joins(:product, line_items: :order)
                     .select("spree_variants.id, spree_products.slug as productId, spree_products.name as name, sku, SUM(spree_line_items.quantity) as quantity, SUM((spree_line_items.price * spree_line_items.quantity) + spree_line_items.adjustment_total) as total_price")
                     .merge(Order.complete.completed_between(completed_at_gt, completed_at_lt))
                     .group("spree_variants.id, spree_products.id, spree_products.name")
         if supports_store_id? && store_id
           @variants = @variants.where("spree_orders.store_id" => store_id)
+        end
+      end
+
+      def total_sales_by_product
+        @products = Spree::Product.joins(:variants, line_items: :order)
+                    .select('spree_products.id, spree_products.name, spree_products.slug, SUM(spree_line_items.quantity) as quantity, SUM((spree_line_items.price * spree_line_items.quantity) + spree_line_items.adjustment_total) as total_price')
+                    .merge(Spree::Order.complete.completed_between(completed_at_gt, completed_at_lt))
+                    .group('spree_products.id, spree_variants.id, spree_products.name')
+                    .uniq
+        if supports_store_id? && store_id
+          @products = @products.where("spree_orders.store_id" => store_id)
         end
       end
 
@@ -55,6 +69,40 @@ module Spree
           .having('sum(spree_stock_items.count_on_hand)<=0')
           .order(orderby)
         @variants = stock_paginate
+      end
+
+      def sales_total_net
+        sales_total
+        @reimbursements = Reimbursement.where('created_at between ? and ?', params[:q][:completed_at_gt], params[:q][:completed_at_lt].present? ? params[:q][:completed_at_lt] : Time.now) 
+        @reimbursements.each do |reimbursement|
+          @totals[reimbursement.order.currency][:reimbursement_total] = ::Money.new(0, reimbursement.order.currency) unless @totals[reimbursement.order.currency][:reimbursement_total]
+          @totals[reimbursement.order.currency][:sales_total_net] = ::Money.new(0, reimbursement.order.currency) unless @totals[reimbursement.order.currency][:sales_total_net]
+          @totals[reimbursement.order.currency][:reimbursement_total] += reimbursement.total.to_money(reimbursement.order.currency)
+          @totals[reimbursement.order.currency][:sales_total_net] = @totals[reimbursement.order.currency][:sales_total] - @totals[reimbursement.order.currency][:reimbursement_total]
+        end
+      end
+
+      def reimbursement_total
+        params[:created_at_gt] = '' unless params[:created_at_gt]
+        params[:created_at_lt] = '' unless params[:created_at_lt]
+
+        if params[:created_at_gt].blank?
+          params[:created_at_gt] = Time.zone.now.beginning_of_month
+        else
+          params[:created_at_gt] = Time.zone.parse(params[:created_at_gt]).beginning_of_day rescue Time.zone.now.beginning_of_month
+        end
+
+        if params[:created_at_lt].blank?
+          params[:created_at_lt] = Time.zone.now
+        else
+          params[:created_at_lt] = Time.zone.parse(params[:created_at_lt]).beginning_of_day rescue Time.zone.now
+        end
+        @reimbursements = Reimbursement.where('created_at between ? and ?', params[:created_at_gt], params[:created_at_lt]) 
+        @totals = {}
+        @reimbursements.each do |reimbursement|
+          @totals[reimbursement.order.currency] = { reimbursement_total: ::Money.new(0, reimbursement.order.currency) } unless @totals[reimbursement.order.currency]
+          @totals[reimbursement.order.currency][:reimbursement_total] += reimbursement.total.to_money(reimbursement.order.currency)
+        end
       end
 
       private
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,20 @@ Spree::Core::Engine.routes.draw do
   namespace :admin do
     resources :reports, only: [:index] do
       collection do
-        get :total_sales_of_each_product
-        post :total_sales_of_each_product
+        get :total_sales_of_each_variant
+        post :total_sales_of_each_variant
         get :ten_days_order_count
         get :thirty_days_order_count
         get :stock_report
         post :stock_report
         get :stockout_report
         post :stockout_report
+        get :sales_total_net
+        post :sales_total_net
+        get :total_sales_by_product
+        post :total_sales_by_product
+        get :reimbursement_total
+        post :reimbursement_total
       end
     end
   end
